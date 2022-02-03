5a5d93320fb64fe9e66532802b1b09e438914743
Giorgi Mosemgvdlishvili <g_mosemgvdlishvili@yahoo.com>
2022-02-03 11:08:59 +0400

    create env

--- lib/apollo.ts
+++ lib/apollo.ts
@@ -1,10 +1,7 @@
 import {ApolloClient,HttpLink, InMemoryCache} from "@apollo/client";
 
-// const isBrowser = typeof window !== 'undefined';
-// const authCookie = (isBrowser) ? Cookie.get('idToken') : null;
-
 const apolloClient = new ApolloClient({
-    uri: "http://localhost:1337/graphql",
+    uri: process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT,
     cache: new InMemoryCache()
 })
 export default apolloClient
